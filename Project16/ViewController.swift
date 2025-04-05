690506e138073511caf6d7cff02426a354b3a7e1
Maksim Li <maximlee.ml@gmail.com>
2025-04-05 23:22:40 +0300

    New changes

--- Project16/ViewController.swift
+++ Project16/ViewController.swift
@@ -7,6 +7,7 @@
 
 import MapKit
 import UIKit
+import WebKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
@@ -15,27 +16,32 @@ class ViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         
         let london = Capital (title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
-        
         let oslo = Capital (title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded a thousand years ago")
-        
         let paris = Capital (title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often call the City of Love")
-        
         let rome = Capital (title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it")
-        
         let washington = Capital (title: "Washington", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "The capital of the USA")
         
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        let mapTypeAlert = UIAlertController(title: "Map View", message: "Choose map type", preferredStyle: .actionSheet)
+        mapTypeAlert.addAction(UIAlertAction(title: "Standard", style: .default) { [weak self] _ in self?.mapView.mapType = .standard})
+        mapTypeAlert.addAction(UIAlertAction(title: "Satellite", style: .default) { [weak self] _ in self?.mapView.mapType = .satellite })
+        mapTypeAlert.addAction(UIAlertAction(title: "Hybrid", style: .default) { [weak self] _ in self?.mapView.mapType = .hybrid })
+        mapTypeAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(mapTypeAlert, animated: true)
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
         
-        let indentifier = "Capital"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: indentifier)
+        let identifier = "Capital"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
         
         if annotationView == nil {
-            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: indentifier)
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
+            annotationView?.markerTintColor = .systemPurple
             
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
@@ -44,13 +50,32 @@ class ViewController: UIViewController, MKMapViewDelegate {
         }
         return annotationView
     }
+    
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let capital = view.annotation as? Capital else { return }
-        let placeName = capital.title
-        let placeInfo = capital.info
         
-        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        let placeName = capital.title ?? "Wikipedia"
+        let urlString = "https://en.wikipedia.org/wiki/\(placeName.replacingOccurrences(of: " ", with: "_"))"
+        let webVC = WebViewController()
+        webVC.urlString = urlString
+        navigationController?.pushViewController(webVC, animated: true)
+    }
+}
+
+class WebViewController: UIViewController {
+    var urlString: String?
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        webView = WKWebView(frame: view.bounds)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
+        
+        if let urlString = urlString, let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 }
